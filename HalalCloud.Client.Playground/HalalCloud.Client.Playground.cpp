36845b29bf14f15b13a03755a13c4f8c6408ad38
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-18 13:36:03 +0800

    Add HalalCloud.Client.Playground project.

---
+++ HalalCloud.Client.Playground/HalalCloud.Client.Playground.cpp
@@ -0,0 +1,29 @@
+﻿/*
+ * PROJECT:   HalalCloud.Client.Playground
+ * FILE:      HalalCloud.Client.Playground.cpp
+ * PURPOSE:   Implementation for Halal Cloud Client Playground
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <clocale>
+#include <cstdio>
+#include <cwchar>
+
+int main()
+{
+    // I can't use that because of the limitation in VC-LTL.
+    // std::setlocale(LC_ALL, "zh_CN.UTF-8");
+
+    std::setlocale(LC_ALL, "chs");
+
+    std::wprintf(
+        L"Halal Cloud Client Playground\n"
+        L"================================================================\n"
+        L"他喵的 MSVC 2019 工具集坑了我们价值百万美刀的项目！\n"
+        L"The F@cking MSVC 2019 toolset ruined our $1M project!\n");
+
+    return 0;
+}
