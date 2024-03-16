881e11e7734a0735a4a1dde5d350614e0680202f
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-16 17:36:33 +0800

    Remove gRPC and vcpkg integration for HalalCloud.Client.Playground project.

--- HalalCloud.Client.Playground/HalalCloud.Client.Playground.cpp
+++ HalalCloud.Client.Playground/HalalCloud.Client.Playground.cpp
@@ -12,10 +12,6 @@
 #include <cstdio>
 #include <cwchar>
 
-#include <grpcpp/grpcpp.h>
-
-#include "../HalalCloud.Protocol/Generated/user_svc.grpc.pb.h"
-
 int main()
 {
     // I can't use that because of the limitation in VC-LTL.
@@ -28,18 +24,6 @@ int main()
         L"================================================================\n"
         L"他喵的 MSVC 2019 工具集坑了我们价值百万美刀的项目！\n"
         L"The F@cking MSVC 2019 toolset ruined our $1M project!\n");
-
-    auto UserClient = v6::services::pub::PubUser::NewStub(grpc::CreateChannel(
-        "https://grpcuserapi.2dland.cn",
-        grpc::SslCredentials(grpc::SslCredentialsOptions())));
-
-    grpc::ClientContext Context;
-    v6::services::pub::LoginRequest Request;
-    Request.set_return_type(2);
-    v6::services::pub::OauthTokenResponse Response;
-    UserClient->CreateAuthToken(&Context, Request, &Response);
-
-    std::printf("%s\n", Response.url().c_str());
 
     return 0;
 }
