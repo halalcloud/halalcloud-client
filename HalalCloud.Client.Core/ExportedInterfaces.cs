2916254e671d0348dc1c7864abfbb2e14d9d20fe
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-28 23:02:47 +0800

    Remove unused implementations for HalalCloud.Client.Core.ExportedInterfaces.

--- HalalCloud.Client.Core/ExportedInterfaces.cs
+++ HalalCloud.Client.Core/ExportedInterfaces.cs
@@ -1,44 +1,10 @@
-﻿using Grpc.Core;
-using Grpc.Core.Interceptors;
-using Grpc.Net.Client;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Runtime.InteropServices;
 using V6.Services.Pub;
 
 namespace HalalCloud.Client.Core
 {
     public class ExportedInterfaces
     {
-        [UnmanagedCallersOnly(EntryPoint = "HccCreateInvoker")]
-        public static IntPtr CreateInvoker()
-        {
-            GrpcChannel Channel = GrpcChannel.ForAddress(
-                "https://grpcuserapi.2dland.cn");
-
-            CallInvoker Invoker = Channel.Intercept(
-                new SignatureInterceptor());
-
-            return GCHandle.ToIntPtr(
-                GCHandle.Alloc(Invoker, GCHandleType.Pinned));
-        }
-
-        [UnmanagedCallersOnly(EntryPoint = "HccFreeInvoker")]
-        public static void FreeInvoker(IntPtr Invoker)
-        {
-            GCHandle.FromIntPtr(Invoker).Free();
-        }
-
-        private static CallInvoker? GetInvoker(IntPtr Invoker)
-        {
-            return GCHandle.FromIntPtr(Invoker).Target as CallInvoker;
-        }
-
-        //[UnmanagedCallersOnly(EntryPoint = "HccCreateAuthToken")]
-        //public static int CreateAuthToken(int a, int b)
-        //{
-        //    return a + b;
-        //}
-
         [UnmanagedCallersOnly(EntryPoint = "HccCreateSessionManager")]
         public static unsafe int CreateSessionManager(
             IntPtr* Interface)
