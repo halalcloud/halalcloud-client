e0fd15a5fa8519c4349c7bdc20663a178407bb3c
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-28 23:02:14 +0800

    Implement HccCreateSessionManager and HccCreateAuthToken.

--- HalalCloud.Client.Core/ExportedInterfaces.cs
+++ HalalCloud.Client.Core/ExportedInterfaces.cs
@@ -38,5 +38,63 @@ namespace HalalCloud.Client.Core
         //{
         //    return a + b;
         //}
+
+        [UnmanagedCallersOnly(EntryPoint = "HccCreateSessionManager")]
+        public static unsafe int CreateSessionManager(
+            IntPtr* Interface)
+        {
+            try
+            {
+                if (Interface == null)
+                {
+                    throw new ArgumentException();
+                }
+
+                *Interface = GCHandle.ToIntPtr(GCHandle.Alloc(
+                    new SessionManager(),
+                    GCHandleType.Pinned));
+                return 0;
+            }
+            catch (Exception e)
+            {
+                *Interface = IntPtr.Zero;
+                return e.HResult;
+            }
+        }
+
+        private static SessionManager? GetSessionManager(
+            IntPtr Interface)
+        {
+            return GCHandle.FromIntPtr(Interface).Target as SessionManager;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "HccCreateAuthToken")]
+        public static unsafe int CreateAuthToken(
+            IntPtr Interface)
+        {
+            try
+            {
+                if (Interface == IntPtr.Zero)
+                {
+                    throw new ArgumentException();
+                }
+
+                SessionManager? Session = GetSessionManager(Interface);
+                if (Session == null)
+                {
+                    throw new ArgumentException();
+                }
+
+                OauthTokenResponse Response = Session.CreateAuthToken();
+                Console.WriteLine(Response.ReturnUrl);
+                Console.WriteLine(Response.Callback);
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                return e.HResult;
+            }
+        }
     }
 }
