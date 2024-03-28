7ab10192b250f9e7536af087a95ff6d16c8a1025
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-28 12:01:37 +0800

    Create HalalCloud.Client.SharpPlayground project.

---
+++ HalalCloud.Client.SharpPlayground/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HalalCloud.Client.SharpPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
