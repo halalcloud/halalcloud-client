be2c95957c8fd687f1016668342235d47bdf8d4f
MouriNaruto <Kenji.Mouri@outlook.com>
2024-04-12 14:58:52 +0800

    Implement HccFree* functions.

--- HalalCloud.Client.Playground/HalalCloud.Client.Core.Native.cpp
+++ HalalCloud.Client.Playground/HalalCloud.Client.Core.Native.cpp
@@ -9,3 +9,88 @@
  */
 
 #include "HalalCloud.Client.Core.Native.h"
+
+#include <cstring>
+
+EXTERN_C VOID WINAPI HccFreeOauthTokenResponse(
+    PHCC_OAUTH_TOKEN_RESPONSE Value)
+{
+    if (Value)
+    {
+        ::CoTaskMemFree(Value->Url);
+        ::CoTaskMemFree(Value->Addon);
+        ::CoTaskMemFree(Value->Input);
+        ::CoTaskMemFree(Value->Type);
+        ::CoTaskMemFree(Value->Callback);
+        ::CoTaskMemFree(Value->ReturnUrl);
+        ::CoTaskMemFree(Value->Captcha);
+        ::CoTaskMemFree(Value->State);
+        std::memset(Value, 0, sizeof(HCC_OAUTH_TOKEN_RESPONSE));
+    }
+}
+
+EXTERN_C VOID WINAPI HccFreeToken(
+    PHCC_TOKEN Value)
+{
+    if (Value)
+    {
+        ::CoTaskMemFree(Value->Identity);
+        ::CoTaskMemFree(Value->UserIdentity);
+        ::CoTaskMemFree(Value->Device);
+        ::CoTaskMemFree(Value->AccessToken);
+        ::CoTaskMemFree(Value->RefreshToken);
+        std::memset(Value, 0, sizeof(HCC_TOKEN));
+    }
+}
+
+EXTERN_C VOID WINAPI HccFreeUser(
+    PHCC_USER Value)
+{
+    if (Value)
+    {
+        ::CoTaskMemFree(Value->Identity);
+        ::CoTaskMemFree(Value->Password);
+        ::CoTaskMemFree(Value->Name);
+        ::CoTaskMemFree(Value->Addon);
+        ::CoTaskMemFree(Value->Hash);
+        ::CoTaskMemFree(Value->Icon);
+        std::memset(Value, 0, sizeof(HCC_USER));
+    }
+}
+
+EXTERN_C VOID WINAPI HccFreeLastLoginResponse(
+    PHCC_LAST_LOGIN_RESPONSE Value)
+{
+    if (Value)
+    {
+        ::CoTaskMemFree(Value->Identity);
+        ::CoTaskMemFree(Value->LastLoginIp);
+        ::CoTaskMemFree(Value->LastLoginDevice);
+        std::memset(Value, 0, sizeof(HCC_LAST_LOGIN_RESPONSE));
+    }
+}
+
+EXTERN_C VOID WINAPI HccFreeLoginResponse(
+    PHCC_LOGIN_RESPONSE Value)
+{
+    if (Value)
+    {
+        ::HccFreeToken(&Value->Token);
+        ::HccFreeUser(&Value->User);
+        ::HccFreeLastLoginResponse(&Value->LastLogin);
+        ::CoTaskMemFree(Value->State);
+        std::memset(Value, 0, sizeof(HCC_LOGIN_RESPONSE));
+    }
+}
+
+EXTERN_C VOID WINAPI HccFreeOauthTokenCheckResponse(
+    PHCC_OAUTH_TOKEN_CHECK_RESPONSE Value)
+{
+    if (Value)
+    {
+        ::HccFreeLoginResponse(&Value->Login);
+        ::HccFreeOauthTokenResponse(&Value->Oauth);
+        ::CoTaskMemFree(Value->Message);
+        std::memset(Value, 0, sizeof(HCC_OAUTH_TOKEN_CHECK_RESPONSE));
+    }
+}
--- HalalCloud.Client.Playground/HalalCloud.Client.Core.Native.h
+++ HalalCloud.Client.Playground/HalalCloud.Client.Core.Native.h
@@ -24,16 +24,19 @@ EXTERN_C HRESULT WINAPI HccCloseSessionManager(
 
 typedef struct _HCC_OAUTH_TOKEN_RESPONSE
 {
-    LPSTR Url; // Free via CoTaskMemFree.
-    LPSTR Addon; // Free via CoTaskMemFree.
-    LPSTR Input; // Free via CoTaskMemFree.
-    LPSTR Type; // Free via CoTaskMemFree.
-    LPSTR Callback; // Free via CoTaskMemFree.
-    LPSTR ReturnUrl; // Free via CoTaskMemFree.
+    LPSTR Url;
+    LPSTR Addon;
+    LPSTR Input;
+    LPSTR Type;
+    LPSTR Callback;
+    LPSTR ReturnUrl;
     INT32 ReturnType;
-    LPSTR Captcha; // Free via CoTaskMemFree.
-    LPSTR State; // Free via CoTaskMemFree.
+    LPSTR Captcha;
+    LPSTR State;
 } HCC_OAUTH_TOKEN_RESPONSE, *PHCC_OAUTH_TOKEN_RESPONSE;
+
+EXTERN_C VOID WINAPI HccFreeOauthTokenResponse(
+    PHCC_OAUTH_TOKEN_RESPONSE Value);
 
 EXTERN_C HRESULT WINAPI HccCreateAuthToken(
     _In_ HCC_SESSION Session,
@@ -41,58 +44,72 @@ EXTERN_C HRESULT WINAPI HccCreateAuthToken(
 
 typedef struct _HCC_TOKEN
 {
-    LPSTR Identity; // Free via CoTaskMemFree.
-    LPSTR UserIdentity; // Free via CoTaskMemFree.
-    LPSTR Device; // Free via CoTaskMemFree.
+    LPSTR Identity;
+    LPSTR UserIdentity;
+    LPSTR Device;
     INT32 Version;
-    LPSTR AccessToken; // Free via CoTaskMemFree.
-    LPSTR RefreshToken; // Free via CoTaskMemFree.
+    LPSTR AccessToken;
+    LPSTR RefreshToken;
     INT64 UpdateTs;
     INT64 AccessTokenExpireTs;
     INT64 RefreshTokenExpireTs;
 } HCC_TOKEN, *PHCC_TOKEN;
 
+EXTERN_C VOID WINAPI HccFreeToken(
+    PHCC_TOKEN Value);
+
 typedef struct _HCC_USER
 {
     // identity for user, unique in system
-    // Free via CoTaskMemFree.
     LPSTR Identity;
     // 1: user, 2: admin, 3: super admin
     INT32 Type;
     // 1: normal, 2: disabled, 3: deleted
     INT32 Status;
     INT64 UpdateTs;
-    LPSTR Password; // Free via CoTaskMemFree.
-    LPSTR Name; // Free via CoTaskMemFree.
-    LPSTR Addon; // Free via CoTaskMemFree.
+    LPSTR Password;
+    LPSTR Name;
+    LPSTR Addon;
     INT64 CreateTs;
-    LPSTR Hash; // Free via CoTaskMemFree.
-    LPSTR Icon; // Free via CoTaskMemFree.
+    LPSTR Hash;
+    LPSTR Icon;
 } HCC_USER, *PHCC_USER;
+
+EXTERN_C VOID WINAPI HccFreeUser(
+    PHCC_USER Value);
 
 typedef struct _HCC_LAST_LOGIN_RESPONSE
 {
-    LPSTR Identity; // Free via CoTaskMemFree.
+    LPSTR Identity;
     INT64 LastLoginTs;
-    LPSTR LastLoginIp; // Free via CoTaskMemFree.
-    LPSTR LastLoginDevice; // Free via CoTaskMemFree.
+    LPSTR LastLoginIp;
+    LPSTR LastLoginDevice;
 } HCC_LAST_LOGIN_RESPONSE, *PHCC_LAST_LOGIN_RESPONSE;
+
+EXTERN_C VOID WINAPI HccFreeLastLoginResponse(
+    PHCC_LAST_LOGIN_RESPONSE Value);
 
 typedef struct _HCC_LOGIN_RESPONSE
 {
     HCC_TOKEN Token;
     HCC_USER User;
     HCC_LAST_LOGIN_RESPONSE LastLogin;
-    LPSTR State; // Free via CoTaskMemFree.
+    LPSTR State;
 } HCC_LOGIN_RESPONSE, *PHCC_LOGIN_RESPONSE;
+
+EXTERN_C VOID WINAPI HccFreeLoginResponse(
+    PHCC_LOGIN_RESPONSE Value);
 
 typedef struct _HCC_OAUTH_TOKEN_CHECK_RESPONSE
 {
     HCC_LOGIN_RESPONSE Login;
     HCC_OAUTH_TOKEN_RESPONSE Oauth;
     INT32 Status;
-    LPSTR Message; // Free via CoTaskMemFree.
+    LPSTR Message;
 } HCC_OAUTH_TOKEN_CHECK_RESPONSE, *PHCC_OAUTH_TOKEN_CHECK_RESPONSE;
+
+EXTERN_C VOID WINAPI HccFreeOauthTokenCheckResponse(
+    PHCC_OAUTH_TOKEN_CHECK_RESPONSE Value);
 
 EXTERN_C HRESULT WINAPI HccVerifyAuthToken(
     _In_ HCC_SESSION Session,
