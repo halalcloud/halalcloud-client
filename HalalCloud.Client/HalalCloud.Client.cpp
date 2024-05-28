f470d6e25862b3ca3349e77e41e148c522d1798a
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-28 18:28:51 +0800

    Add HalalCloud.Client project.

---
+++ HalalCloud.Client/HalalCloud.Client.cpp
@@ -0,0 +1,31 @@
+﻿/*
+ * PROJECT:   Halal Cloud Client
+ * FILE:      HalalCloud.Client.cpp
+ * PURPOSE:   Implementation for Halal Cloud Client
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nShowCmd);
+
+    ::MessageBoxW(
+        nullptr,
+        L"Hello World!\n",
+        L"Halal Cloud Client",
+        0);
+
+    return 0;
+}
