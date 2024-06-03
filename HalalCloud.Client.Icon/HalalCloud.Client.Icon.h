da2272a224adb6f64e4f4b9cbdec62d4f4374886
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-03 14:33:38 +0800

    Add HalalCloud.Client.Icon project.

---
+++ HalalCloud.Client.Icon/HalalCloud.Client.Icon.h
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Halal Cloud Client
+ * FILE:      HalalCloud.Client.Icon.h
+ * PURPOSE:   Windows icon resource for Halal Cloud Client
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#define IDI_HALALCLOUD_CLIENT 101
