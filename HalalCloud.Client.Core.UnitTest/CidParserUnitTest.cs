6a6538df3bd230be26e5cd0221e4d6cad87cd903
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-09 13:52:03 +0800

    Add unit test for CidParser.ParseCid.

--- HalalCloud.Client.Core.UnitTest/CidParserUnitTest.cs
+++ HalalCloud.Client.Core.UnitTest/CidParserUnitTest.cs
@@ -33,5 +33,27 @@
                 (16384, 3),
                 CidParser.ParseUnsignedVarint(new byte[] { 0x80, 0x80, 0x01, 0x00 }));
         }
+
+        /// <summary>
+        /// Unit test for <see cref="CidParser.ParseCid(string)"/>.
+        /// </summary>
+        [TestMethod]
+        public void TestParseCid()
+        {
+            Assert.AreEqual(
+                "C3C4733EC8AFFD06CF9E9FF50FFC6BCD2EC85A6170004BB709669C31DE94391A",
+                Utilities.ToHashString(CidParser.ParseCid(
+                    "bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi").HashValue));
+
+            Assert.AreEqual(
+                "E5D8A2FF507EF9A8F88FE8634137CEFDE185D55D45E21B9201D5BC7DF09DDA70",
+                Utilities.ToHashString(CidParser.ParseCid(
+                    "bafybeihf3crp6ud67guprd7imnatptx54gc5kxkf4inzeaovxr67bho2oa").HashValue));
+
+            Assert.AreEqual(
+                "61A92E0904D6DB8850E208090BB243623C7EBADA4A47F5F894335AB35D31F651",
+                Utilities.ToHashString(CidParser.ParseCid(
+                    "bafkreidbvexasbgw3oefbyqibef3eq3chr7lvwski727rfbtlkzv2mpwke").HashValue));
+        }
     }
 }
