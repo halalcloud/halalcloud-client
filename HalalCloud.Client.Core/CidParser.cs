3a57164a2d5e677294cfa3fa534e0827dd587e30
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-09 12:59:15 +0800

    Move MultibaseEncoding type from HalalCloud.Client.Core.CidParser to HalalCloud.Client.Core.

--- HalalCloud.Client.Core/CidParser.cs
+++ HalalCloud.Client.Core/CidParser.cs
@@ -3,62 +3,6 @@
     public class CidParser
     {
         /// <summary>
-        /// The type of multibase encoding.
-        /// </summary>
-        public enum MultibaseEncoding
-        {
-            /// <summary>
-            /// No base encoding
-            /// </summary>
-            None,
-
-            /// <summary>
-            /// Unsupported base encoding
-            /// </summary>
-            Unsupported,
-
-            /// <summary>
-            /// Hexadecimal (lowercase)
-            /// </summary>
-            Base16,
-
-            /// <summary>
-            /// Hexadecimal (uppercase)
-            /// </summary>
-            Base16Upper,
-
-            /// <summary>
-            /// RFC4648 case-insensitive - no padding (lowercase)
-            /// </summary>
-            Base32,
-
-            /// <summary>
-            /// RFC4648 case-insensitive - no padding (uppercase)
-            /// </summary>
-            Base32Upper,
-
-            /// <summary>
-            /// Base58 Bitcoin
-            /// </summary>
-            Base58Btc,
-
-            /// <summary>
-            /// RFC4648 no padding
-            /// </summary>
-            Base64,
-
-            /// <summary>
-            /// RFC4648 no padding
-            /// </summary>
-            Base64Url,
-
-            /// <summary>
-            /// RFC4648 with padding
-            /// </summary>
-            Base64UrlPad
-        }
-
-        /// <summary>
         /// Parse a multibase string.
         /// </summary>
         /// <param name="InputString">The input multibase string.</param>
