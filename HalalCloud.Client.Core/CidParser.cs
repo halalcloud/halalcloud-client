e251f6035118d1e13cf35922a81a889761a04abf
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-09 12:56:45 +0800

    Add CidParser.MultibaseEncoding to HalalCloud.Client.Core.

--- HalalCloud.Client.Core/CidParser.cs
+++ HalalCloud.Client.Core/CidParser.cs
@@ -3,6 +3,62 @@
     public class CidParser
     {
         /// <summary>
+        /// The type of multibase encoding.
+        /// </summary>
+        public enum MultibaseEncoding
+        {
+            /// <summary>
+            /// No base encoding
+            /// </summary>
+            None,
+
+            /// <summary>
+            /// Unsupported base encoding
+            /// </summary>
+            Unsupported,
+
+            /// <summary>
+            /// Hexadecimal (lowercase)
+            /// </summary>
+            Base16,
+
+            /// <summary>
+            /// Hexadecimal (uppercase)
+            /// </summary>
+            Base16Upper,
+
+            /// <summary>
+            /// RFC4648 case-insensitive - no padding (lowercase)
+            /// </summary>
+            Base32,
+
+            /// <summary>
+            /// RFC4648 case-insensitive - no padding (uppercase)
+            /// </summary>
+            Base32Upper,
+
+            /// <summary>
+            /// Base58 Bitcoin
+            /// </summary>
+            Base58Btc,
+
+            /// <summary>
+            /// RFC4648 no padding
+            /// </summary>
+            Base64,
+
+            /// <summary>
+            /// RFC4648 no padding
+            /// </summary>
+            Base64Url,
+
+            /// <summary>
+            /// RFC4648 with padding
+            /// </summary>
+            Base64UrlPad
+        }
+
+        /// <summary>
         /// Parse an integer from a byte array with unsigned varint (VARiable
         /// INTeger) format.
         /// </summary>
