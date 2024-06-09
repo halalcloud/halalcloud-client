85a889eb51cd1c0a6d5d42f329887bb8f1ce654e
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-09 12:07:22 +0800

    Add CidParser.ParseUnsignedVarint to HalalCloud.Client.Core.

---
+++ HalalCloud.Client.Core.UnitTest/CidParserUnitTest.cs
@@ -0,0 +1,37 @@
+﻿namespace HalalCloud.Client.Core.UnitTest
+{
+    [TestClass]
+    public class CidParserUnitTest
+    {
+        /// <summary>
+        /// Unit test for <see cref="CidParser.ParseUnsignedVarint(byte[])"/>.
+        /// </summary>
+        [TestMethod]
+        public void TestParseUnsignedVarint()
+        {
+            // Reference: https://github.com/multiformats/unsigned-varint#spec
+
+            Assert.AreEqual(
+                (1, 1),
+                CidParser.ParseUnsignedVarint(new byte[] { 0x01 }));
+            Assert.AreEqual(
+                (127, 1),
+                CidParser.ParseUnsignedVarint(new byte[] { 0x7F }));
+            Assert.AreEqual(
+                (128, 2),
+                CidParser.ParseUnsignedVarint(new byte[] { 0x80, 0x01 }));
+            Assert.AreEqual(
+                (255, 2),
+                CidParser.ParseUnsignedVarint(new byte[] { 0xFF, 0x01 }));
+            Assert.AreEqual(
+                (300, 2),
+                CidParser.ParseUnsignedVarint(new byte[] { 0xAC, 0x02 }));
+            Assert.AreEqual(
+                (16384, 3),
+                CidParser.ParseUnsignedVarint(new byte[] { 0x80, 0x80, 0x01 }));
+            Assert.AreEqual(
+                (16384, 3),
+                CidParser.ParseUnsignedVarint(new byte[] { 0x80, 0x80, 0x01, 0x00 }));
+        }
+    }
+}
---
+++ HalalCloud.Client.Core/CidParser.cs
@@ -0,0 +1,40 @@
+﻿namespace HalalCloud.Client.Core
+{
+    public class CidParser
+    {
+        /// <summary>
+        /// Parse an integer from a byte array with unsigned varint (VARiable
+        /// INTeger) format.
+        /// </summary>
+        /// <param name="InputBytes">
+        /// The byte array with unsigned varint (VARiable INTeger) format.
+        /// </param>
+        /// <returns>The parsed integer.</returns>
+        /// <see cref="https://github.com/multiformats/unsigned-varint"/>
+        /// <remarks>
+        /// For security, to avoid memory attacks, we use a "practical max" of
+        /// 9 bytes. Though there is no theoretical limit, and future specs can
+        /// grow this number if it is truly necessary to have code or length
+        /// values equal to or larger than 2^63.
+        /// </remarks>
+        public static (long Integer, int Parsed) ParseUnsignedVarint(
+            byte[] InputBytes)
+        {
+            const int MaximumBytes = 9;
+
+            (long Integer, int Parsed) Result = (0, 0);
+            for (int i = 0; i < InputBytes.Length && i < MaximumBytes; ++i)
+            {
+                Result.Integer |=
+                    (long)(InputBytes[i] & 0x7F) << (Result.Parsed * 7);
+                ++Result.Parsed;
+                if ((InputBytes[i] & 0x80) == 0)
+                {
+                    break;
+                }
+            }
+
+            return Result;
+        }
+    }
+}
