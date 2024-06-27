8d16c963bb63a49e4b59bb794e6493eee553f383
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-27 15:00:04 +0800

    Add HalalCloud.Client.Core.FileAttributes.

---
+++ HalalCloud.Client.Core/FileAttributes.cs
@@ -0,0 +1,8 @@
+﻿namespace HalalCloud.Client.Core
+{
+    public class FileAttributes
+    {
+        public const uint Hidden = 0x00000002;
+        public const uint Directory = 0x00000010;
+    }
+}
