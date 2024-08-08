2fcc18557c23dfba400f29b0dc5bab898ad51e1d
MouriNaruto <Kenji.Mouri@outlook.com>
2024-08-08 21:25:25 +0800

    Add Global.RootPath to HalalCloud.Client.Core.

---
+++ HalalCloud.Client.Core/Global.cs
@@ -0,0 +1,19 @@
+﻿namespace HalalCloud.Client.Core
+{
+    public class Global
+    {
+        private static readonly string CachedRootPath = Path.Combine(
+             Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData),
+             "HalalCloud");
+
+        public static string RootPath
+        {
+            get
+            {
+                Utilities.EnsurePathExists(CachedRootPath);
+                return CachedRootPath;
+            }
+        }
+    }
+}
