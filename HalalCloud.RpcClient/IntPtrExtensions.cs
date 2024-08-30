2716f0892be8cf4cd34afc6f8429e27f2b5ec649
MouriNaruto <Kenji.Mouri@outlook.com>
2024-08-31 02:36:37 +0800

    Implement HalalCloud.RpcClient.IntPtrExtensions.

---
+++ HalalCloud.RpcClient/IntPtrExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HalalCloud.RpcClient
+{
+    public static class IntPtrExtensions
+    {
+        public static GCHandle ToGcHandle(
+            this object Value)
+        {
+            return GCHandle.Alloc(Value);
+        }
+
+        public static GCHandle ToGcHandle(
+            this IntPtr Value)
+        {
+            return GCHandle.FromIntPtr(Value);
+        }
+
+        public static IntPtr ToIntPtr(
+            this object Value)
+        {
+            return GCHandle.ToIntPtr(Value.ToGcHandle());
+        }
+
+        public static T ToObject<T>(
+            this IntPtr Value)
+        {
+            T? Object = (T?)Value.ToGcHandle().Target;
+            if (Object == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return Object;
+        }
+
+        public static T ToDelegate<T>(
+            this IntPtr Value)
+        {
+            return Marshal.GetDelegateForFunctionPointer<T>(Value);
+        }
+
+        public static string ToUtf8String(
+            this IntPtr Value)
+        {
+            return Marshal.PtrToStringUTF8(Value) ?? string.Empty;
+        }
+    }
+}
