39b123883f25558aaec7921cc7f0ad9809ed0b25
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-22 23:59:41 +0800

    Add HalalCloud.Client.Session.

---
+++ HalalCloud.Client.Playground/HalalCloud.Client.Session.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Halal Cloud Client
+ * FILE:      HalalCloud.Client.Session.cpp
+ * PURPOSE:   Implementation for the HalalCloud.Client.Session
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HalalCloud.Client.Session.h"
---
+++ HalalCloud.Client.Playground/HalalCloud.Client.Session.h
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:   Halal Cloud Client
+ * FILE:      HalalCloud.Client.Session.h
+ * PURPOSE:   Definition for the HalalCloud.Client.Session
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_CLIENT_SESSION
+#define HALALCLOUD_CLIENT_SESSION
+
+#endif // !HALALCLOUD_CLIENT_SESSION
