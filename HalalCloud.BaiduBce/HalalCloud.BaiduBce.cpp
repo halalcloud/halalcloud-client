f6249604bfb227671be86fcba06eab7b05feeaa9
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-26 16:27:12 +0800

    Add HccBcePutObject to HalalCloud.BaiduBce.

---
+++ HalalCloud.BaiduBce/HalalCloud.BaiduBce.cpp
@@ -0,0 +1,36 @@
+﻿/*
+ * PROJECT:   Halal Cloud Client
+ * FILE:      HalalCloud.BaiduBce.cpp
+ * PURPOSE:   Implementation for the HalalCloud.BaiduBce
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HalalCloud.BaiduBce.h"
+
+#include "bcesdk/bos/client.h"
+
+extern "C" int HccBcePutObject(
+    const char* FilePath,
+    const char* Endpoint,
+    const char* AccessKey,
+    const char* SecretKey,
+    const char* Token,
+    const char* Bucket,
+    const char* Key)
+{
+    baidu::bos::cppsdk::Credential Credential(
+        AccessKey,
+        SecretKey,
+        Token);
+    baidu::bos::cppsdk::ClientOptions Options;
+    Options.endpoint = Endpoint;
+    Options.max_parallel = 5;
+    baidu::bos::cppsdk::Client Client(Credential, Options);
+    baidu::bos::cppsdk::FileInputStream FileStream(FilePath);
+    baidu::bos::cppsdk::PutObjectRequest Request(Bucket, Key, &FileStream);
+    baidu::bos::cppsdk::PutObjectResponse Response;
+    return Client.put_object(Request, &Response);
+}
