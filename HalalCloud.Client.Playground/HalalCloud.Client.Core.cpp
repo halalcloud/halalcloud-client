97a7f1a2b67749b886d5aa3b677a9620deb4b2dc
MouriNaruto <Kenji.Mouri@outlook.com>
2025-06-15 16:33:07 +0800

    Add HalalCloud.Client.Core.

---
+++ HalalCloud.Client.Playground/HalalCloud.Client.Core.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HalalCloud.Client.Coren.h
+ * PURPOSE:    Definition for the HalalCloud.Client.Core
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+// Ensure the sal.h which is included by Windows C runtime used by
+// Mile.Helpers.CppBase.h will be included before Mile.Mobility.Portable.Types.h
+// which is included by HalalCloud.Client.Core.h.
+#include <Mile.Helpers.CppBase.h>
+
+#include "HalalCloud.Client.Core.h"
---
+++ HalalCloud.Client.Playground/HalalCloud.Client.Core.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HalalCloud.Client.Core.h
+ * PURPOSE:    Definition for the HalalCloud.Client.Core
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_CLIENT_CORE
+#define HALALCLOUD_CLIENT_CORE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !HALALCLOUD_CLIENT_CORE
