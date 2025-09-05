7f4f435e7cb692c70ffb8c1d82fa0ea751ac022c
MouriNaruto <Kenji.Mouri@outlook.com>
2025-09-05 16:12:31 +0800

    Create HalalCloud.Specification.Multiformats.

---
+++ HalalCloud.Client.Playground/HalalCloud.Specification.Multiformats.h
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HalalCloud.Specification.Multiformats.h
+ * PURPOSE:    Definition for Multiformats Specification
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_SPECIFICATION_MULTIFORMATS
+#define HALALCLOUD_SPECIFICATION_MULTIFORMATS
+
+#endif // !HALALCLOUD_SPECIFICATION_MULTIFORMATS
