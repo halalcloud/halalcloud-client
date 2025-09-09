f56d30807c66dd932b34bc7c55312d8b12415082
MouriNaruto <Kenji.Mouri@outlook.com>
2025-09-09 18:55:50 +0800

    Add ThreadPool implementation to HalalCloud.Portable.Helpers.

--- HalalCloud.Client.Playground/HalalCloud.Portable.Helpers.cpp
+++ HalalCloud.Client.Playground/HalalCloud.Portable.Helpers.cpp
@@ -9,3 +9,70 @@
  */
 
 #include "HalalCloud.Portable.Helpers.h"
+
+HalalCloud::ThreadPool::ThreadPool(
+    std::size_t const& MaximumWorkerCount)
+{
+    for (size_t i = 0; i < MaximumWorkerCount; ++i)
+    {
+        this->m_Workers.emplace_back([this]()
+        {
+            for (;;)
+            {
+                std::function<void()> Task;
+                {
+                    std::unique_lock<std::mutex> Lock(this->m_TaskQueueMutex);
+                    this->m_TaskQueueCondition.wait(
+                        Lock,
+                        [this]()
+                    {
+                        return !this->m_Running || !this->m_TaskQueue.empty();
+                    });
+                    if (!this->m_Running && this->m_TaskQueue.empty())
+                    {
+                        return;
+                    }
+                    Task = std::move(this->m_TaskQueue.front());
+                    this->m_TaskQueue.pop();
+                }
+                Task();
+            }
+        });
+    }
+}
+
+HalalCloud::ThreadPool::ThreadPool() :
+    ThreadPool(std::thread::hardware_concurrency())
+{
+}
+
+HalalCloud::ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock<std::mutex> Lock(this->m_TaskQueueMutex);
+        this->m_Running = false;
+    }
+    this->m_TaskQueueCondition.notify_all();
+    for (std::thread& Worker : this->m_Workers)
+    {
+        if (Worker.joinable())
+        {
+            Worker.join();
+        }
+    }
+}
+
+void HalalCloud::ThreadPool::Enqueue(
+    std::function<void()> const& Task)
+{
+    {
+        std::unique_lock<std::mutex> Lock(this->m_TaskQueueMutex);
+        if (!this->m_Running)
+        {
+            throw std::runtime_error(
+                "Unable to enqueue a task to a stopped thread pool.");
+        }
+        this->m_TaskQueue.push(Task);
+    }
+    this->m_TaskQueueCondition.notify_one();
+}
--- HalalCloud.Client.Playground/HalalCloud.Portable.Helpers.h
+++ HalalCloud.Client.Playground/HalalCloud.Portable.Helpers.h
@@ -11,9 +11,38 @@
 #ifndef HALALCLOUD_PORTABLE_HELPERS
 #define HALALCLOUD_PORTABLE_HELPERS
 
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
 namespace HalalCloud
 {
+    class ThreadPool
+    {
+    private:
 
+        std::vector<std::thread> m_Workers;
+        std::queue<std::function<void()>> m_TaskQueue;
+        std::mutex m_TaskQueueMutex;
+        std::condition_variable m_TaskQueueCondition;
+        std::atomic_bool m_Running = true;
+
+    public:
+
+        ThreadPool(
+            std::size_t const& MaximumWorkerCount);
+
+        ThreadPool();
+
+        ~ThreadPool();
+
+        void Enqueue(
+            std::function<void()> const& Task);
+    };
 }
 
 #endif // !HALALCLOUD_PORTABLE_HELPERS
