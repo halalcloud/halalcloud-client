4d64da4bd7b0fc727c70c921aeb996613b999c21
MouriNaruto <Kenji.Mouri@outlook.com>
2025-09-09 18:16:45 +0800

    Create HalalCloud.Portable.Helpers.

---
+++ HalalCloud.Client.Playground/HalalCloud.Portable.Helpers.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HalalCloud.Portable.Helpers.cpp
+ * PURPOSE:    Implementation for Portable Helpers
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HalalCloud.Portable.Helpers.h"
---
+++ HalalCloud.Client.Playground/HalalCloud.Portable.Helpers.h
@@ -0,0 +1,19 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HalalCloud.Portable.Helpers.h
+ * PURPOSE:    Definition for Portable Helpers
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_PORTABLE_HELPERS
+#define HALALCLOUD_PORTABLE_HELPERS
+
+namespace HalalCloud
+{
+
+}
+
+#endif // !HALALCLOUD_PORTABLE_HELPERS
