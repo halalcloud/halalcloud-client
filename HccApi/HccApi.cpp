b0a420e94b0d1cdd5ed518c620b629a9032f1515
MouriNaruto <Kenji.Mouri@outlook.com>
2025-09-24 14:57:20 +0800

    Introduce HccApi project.

---
+++ HccApi/HccApi.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client Library
+ * FILE:       HccApi.cpp
+ * PURPOSE:    Implementation for Halal Cloud Client Library
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HccApi.h"
---
+++ HccApi/HccApi.h
@@ -0,0 +1,27 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client Library
+ * FILE:       HccApi.h
+ * PURPOSE:    Definition for Halal Cloud Client Library
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_CLIENT_API
+#define HALALCLOUD_CLIENT_API
+
+#include <Mile.Mobility.Portable.Types.h>
+#ifndef MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <sal.h>
+#endif // !MILE_MOBILITY_ENABLE_MINIMUM_SAL
+
+// Workaround for Windows SDK, which defines these types in #ifndef VOID block.
+// That design is terrible, but needs to have a workaround.
+#ifdef VOID
+typedef char CHAR;
+typedef short SHORT;
+typedef long LONG;
+#endif // VOID
+
+#endif // !HALALCLOUD_CLIENT_API
