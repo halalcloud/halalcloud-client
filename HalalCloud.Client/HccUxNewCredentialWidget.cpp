dc94a5722fd1c82a4db8e1fbf843d88cbf38d0f1
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-14 16:49:19 +0800

    Introduce HccUxNewCredentialWidget.

---
+++ HalalCloud.Client/HccUxNewCredentialWidget.cpp
@@ -0,0 +1,23 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HccUxNewCredentialWidget.cpp
+ * PURPOSE:    Implementation for New Credential Widget
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HccUxNewCredentialWidget.h"
+
+HccUxNewCredentialWidget::HccUxNewCredentialWidget(
+    QWidget* Parent)
+	: QWidget(Parent)
+{
+    this->setupUi(this);
+}
+
+HccUxNewCredentialWidget::~HccUxNewCredentialWidget()
+{
+
+}
---
+++ HalalCloud.Client/HccUxNewCredentialWidget.h
@@ -0,0 +1,28 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HccUxNewCredentialWidget.h
+ * PURPOSE:    Definition for New Credential Widget
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#pragma once
+
+#include <QWidget>
+#include "ui_HccUxNewCredentialWidget.h"
+
+class HccUxNewCredentialWidget :
+    public QWidget,
+    public Ui::HccUxNewCredentialWidget
+{
+	Q_OBJECT
+
+public:
+
+	HccUxNewCredentialWidget(
+        QWidget* Parent = nullptr);
+
+	~HccUxNewCredentialWidget();
+};
