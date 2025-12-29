cae73f5bf2b895f75f8e7ca9b1bbb467daffb931
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-29 17:13:13 +0800

    Add HalalCloud.Icon project to Assets.

---
+++ Assets/HalalCloud.Icon.h
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Halal Cloud
+ * FILE:       HalalCloud.Icon.h
+ * PURPOSE:    Windows icon resource for Halal Cloud
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#define IDI_HALALCLOUD 101
