e725f42fdac75cf7e38af3198510ed43bd717af9
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-30 18:51:14 +0800

    Add HccProtocolWrappers to HalalCloud.Client. But also add this reference to HalalCloud.Client.Playground.

---
+++ HalalCloud.Client/HccProtocolWrappers.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HccProtocolWrappers.cpp
+ * PURPOSE:    Implementation for Halal Cloud Client Protocol Wrappers
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HccProtocolWrappers.h"
---
+++ HalalCloud.Client/HccProtocolWrappers.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HccProtocolWrappers.h
+ * PURPOSE:    Definition for Halal Cloud Client Protocol Wrappers
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_CLIENT_PROTOCOL_WRAPPERS
+#define HALALCLOUD_CLIENT_PROTOCOL_WRAPPERS
+
+#include <HccApi.h>
+
+#endif // !HALALCLOUD_CLIENT_PROTOCOL_WRAPPERS
