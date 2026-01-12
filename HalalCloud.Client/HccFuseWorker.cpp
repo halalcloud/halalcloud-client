7fb94ebb991ce79b3891942075a949f5fc6ec34e
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-12 16:34:25 +0800

    Add HccFuseWorker.

---
+++ HalalCloud.Client/HccFuseWorker.cpp
@@ -0,0 +1,13 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HccFuseWorker.cpp
+ * PURPOSE:    Implementation for Halal Cloud Client FUSE Worker
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "HccFuseWorker.h"
+
+#include <fuse.h>
---
+++ HalalCloud.Client/HccFuseWorker.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:    Halal Cloud Client
+ * FILE:       HccFuseWorker.h
+ * PURPOSE:    Definition for Halal Cloud Client FUSE Worker
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef HALALCLOUD_CLIENT_FUSE_WORKER
+#define HALALCLOUD_CLIENT_FUSE_WORKER
+
+#include "HccProtocolWrappers.h"
+
+#endif // !HALALCLOUD_CLIENT_FUSE_WORKER
